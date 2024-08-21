499c48319774f134d5adc282f262ece0d615e895
Jiye <1909621145@qq.com>
2024-08-21 16:37:41 +0800

    ETH new

--- scraper.py
+++ scraper.py
@@ -1,62 +1,51 @@
-import random
 import time
 import redis
-import tls_client
+import requests
 
-headers = {
-    'accept': '*/*',
-    'accept-language': 'zh-CN,zh;q=0.9',
-    'cache-control': 'no-cache',
-    'origin': 'https://cn.cointelegraph.com',
-    'pragma': 'no-cache',
-    'priority': 'u=1, i',
-    'referer': 'https://cn.cointelegraph.com/',
-    'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-site',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-}
+# 设置请求的URL和Redis连接信息
+BINANCE_BASE_URL = "https://api.binance.com"
+TICKER_ENDPOINT = "/api/v3/ticker/price"
+SYMBOLS = '["ETHUSDT","BTCUSDT","BNBUSDT","SOLUSDT","ARBUSDT","MEMEUSDT"]'
+
+# Redis连接配置
+REDIS_HOST = 'redis'  # 修改为你的Redis服务器地址
+REDIS_PORT = 6379
+REDIS_DB = 5
 
 
 def update_data(redis_conn, key, value):
+    """更新Redis中的价格数据，保留最近的5条记录"""
     redis_conn.lpush(key, value)
-    redis_conn.ltrim(key, 0, 4)  # 保留最近的5条记录
+    redis_conn.ltrim(key, 0, 4)  # 只保留最新的5条记录
 
 
-def fetch_and_store_data(redis_conn, crypto_symbol, key):
-    http = tls_client.Session(random_tls_extension_order=True)
-    params = {
-        'cryptoSymbol': crypto_symbol,
-        'fiatSymbol': 'USD',
-        'amount': '1',
-        'reversed': 'false',
-    }
-    response = http.get('https://ticker-api.cointelegraph.com/converter/rate', params=params, headers=headers)
-    value = float(response.json()['rate'])
-    update_data(redis_conn, key, value)
-    data = redis_conn.lrange(key, 0, 4)
-    print(f'{crypto_symbol}:', data)
-    time.sleep(5)
+def fetch_and_store_data(redis_conn):
+    """从Binance API获取最新的价格并存储到Redis"""
+    url = f"{BINANCE_BASE_URL}{TICKER_ENDPOINT}?symbols={SYMBOLS}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        prices = response.json()
+        for price_info in prices:
+            symbol = price_info['symbol']
+            price = float(price_info['price'])
+            key = f"{symbol}_values"
+            update_data(redis_conn, key, price)
+            data = redis_conn.lrange(key, 0, 2)
+            print(f'{symbol}:', data)
+    else:
+        print(f"Failed to fetch prices. Status Code: {response.status_code}")
 
 
 def main_loop():
+    """主循环，从Binance API定期获取数据并存储到Redis"""
     try:
-        with redis.Redis(host='redis', port=6379, db=5) as redis_conn:
-        # with redis.Redis(host='192.168.20.250', port=6379, db=5) as redis_conn:
+        with redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB) as redis_conn:
             while True:
-                fetch_and_store_data(redis_conn, 'ETH', 'ETH_values')
-                fetch_and_store_data(redis_conn, 'BTC', 'BTC_values')
-                fetch_and_store_data(redis_conn, 'BNB', 'BNB_values')
-                fetch_and_store_data(redis_conn, 'SOL', 'SOL_values')
-                fetch_and_store_data(redis_conn, 'ARB', 'ARB_values')
-                fetch_and_store_data(redis_conn, 'MEME', 'MEME_values')
-
-                time.sleep(1 * 60)  # 等待1分钟后再次循环
+                fetch_and_store_data(redis_conn)
+                time.sleep(6)  # 等待1分钟后再次循环
     except Exception as e:
-        print(e)
+        print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
